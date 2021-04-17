2a3e36067350b5b0cedf313967ef595faf764139
amandaboujos <amandaboujos@hotmail.com>
2021-04-17 17:24:01 -0400

    seventh commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
+const responsive = {
+    0:{
+        items: 1
+    },
+    320:{ 
+        items: 1
+    },
+    560:{
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+
 $(document).ready(function(){
 
     $nav = $('.nav');
-    $toggleCollapse = $('.toggle-collapse')
+    $toggleCollapse = $('.toggle-collapse');
 
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse');
@@ -13,8 +29,16 @@ $(document).ready(function(){
         autoplayTimeout: 3000,
         dots: false,
         nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+        navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+        responsive: responsive
     });
+
+
+    $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop: 0
+        },1000);
+    })
 
 
 });
